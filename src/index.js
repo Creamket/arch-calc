010d88961a02c5a5f958472842e0c7b75f59c486
Creamket_ <ForWorkNotGame@gmail.com>
2021-07-21 18:07:52 +0300

    Some fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { compose, createStore, applyMiddleware } from 'redux'
+import { compose, createStore } from 'redux'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
 import './index.css'
 import App from './App'
 import { rootReducer } from './redux/rootReducer'
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 const app = (
