64f788d9f604790f1877f6767f64b6968bfc8400
Creamket_ <ForWorkNotGame@gmail.com>
2021-08-10 19:19:09 +0300

    Some fixes

--- src/components/BeamCalculator/Result.js
+++ src/components/BeamCalculator/Result.js
@@ -2,8 +2,8 @@ export default function Result({ result }) {
   return (
     <>
       <h5 className='card-title'>Продольная арматура:</h5>
-      Площадь поперечного сечения стержня: {+result.area.toFixed(4)} см² <br />
-      Диаметр стержня: {result.diameter} мм <br />
+      Площадь поперечного сечения стержня: {+result.area.toFixed(4)} см². <br />
+      Диаметр стержня: {result.diameter} мм. <br />
       Теоретический вес арматуры: {result.weight} кг/м пог.
       {result.scheme === 1 && (
         <>
@@ -12,17 +12,17 @@ export default function Result({ result }) {
             ? 'Поперечной арматуры не требуется.'
             : result.height <= 0.45
             ? `На приопорных участках установить поперечную арматуру с шагом не более ${
-                0.5 * result.height < 0.15 ? 0.5 * result.height : 0.15
-              } м на участке ${0.25 * result.length} м от опоры. `
+                0.5 * result.height < 0.15 ? +(0.5 * result.height).toFixed(3) : 0.15
+              } м на участке ${+(0.25 * result.length).toFixed(3)} м от опоры. `
             : `На приопорных участках установить поперечную арматуру с шагом не более ${
-                0.33 * result.height < 0.5 ? 0.33 * result.height : 0.5
-              } м на участке ${0.25 * result.length} м от опоры. `}
+                0.33 * result.height < 0.5 ? +(0.33 * result.height).toFixed(3) : 0.5
+              } м на участке ${+(0.25 * result.length).toFixed(3)} м от опоры. `}
           <br />
           {!result.cross &&
             `В остальной части пролета ${
               result.height > 0.3
                 ? `установить поперечную арматуру с шагом не более ${
-                    0.75 * result.height < 0.5 ? 0.75 * result.height : 0.5
+                    0.75 * result.height < 0.5 ? +(0.75 * result.height).toFixed(3) : 0.5
                   } м.`
                 : `поперечная арматура не требуется.`
             }`}
